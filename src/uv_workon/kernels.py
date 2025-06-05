3cab64332a5e32c28725708f2f433b8be5d09d92
wpk-nist-gov <wpk@nist.gov>
2025-06-05 16:30:08 -0400

    chore: multiple update cruft

--- src/uv_workon/kernels.py
+++ src/uv_workon/kernels.py
@@ -62,9 +62,7 @@ def remove_kernelspecs(names: list[str]) -> None:
     has_jupyter_client()
     from jupyter_client.kernelspecapp import RemoveKernelSpec
 
-    RemoveKernelSpec(
-        spec_names=names, force=True
-    ).start()  # ty: ignore[missing-argument]
+    RemoveKernelSpec(spec_names=names, force=True).start()  # ty: ignore[missing-argument]
 
 
 def complete_kernelspec_names(incomplete: str) -> Iterator[str]:
