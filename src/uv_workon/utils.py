a780bd85e8b580bfaa29e077d1abfd1256510bbf
wpk-nist-gov <wpk@nist.gov>
2026-03-27 17:19:10 -0400

    chore(template): accept new copier update (#54)
    Co-authored-by: wpk-nist-gov <5348008+wpk-nist-gov@users.noreply.github.com>

--- src/uv_workon/utils.py
+++ src/uv_workon/utils.py
@@ -23,15 +23,13 @@ def select_option(
 
     title_list: list[str] = [title] if title else []
 
-    title = " ".join(
-        [
-            *title_list,
-            *(
-                ["use arrows or j/k to move down/up, or / to limit by name"]
-                if usage
-                else []
-            ),
-        ]
-    )
+    title = " ".join([
+        *title_list,
+        *(
+            ["use arrows or j/k to move down/up, or / to limit by name"]
+            if usage
+            else []
+        ),
+    ])
     index = cast("int", TerminalMenu(options, title=title or None).show())
     return options[index]
